f7a7686c2d475be353b73312ee84a91fb3fe3b9e
Clever Santoro <cleversantoro@gmail.com>
2026-04-19 00:49:47 -0300

    aajuste página clientes

---
+++ src/app/features/clientes/pages/cliente-editar/cliente-editar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-editar',
+  imports: [],
+  templateUrl: './cliente-editar.html',
+  styleUrl: './cliente-editar.scss',
+})
+export class ClienteEditar {
+
+}
